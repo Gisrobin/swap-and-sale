7249d3cabe378528a526575e4becf764d64b17bb
Gisrobin <gisrobinezeobi@gmail.com>
2025-06-05 14:07:01 -0700

    merged

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
 import Signin from './Components/Signin';
 import './index.css';
+import logo from './logo.svg';
 // import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-          <h1>swap and sale</h1>
-      
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Signin/>
   );
 }
 
