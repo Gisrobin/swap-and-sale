8249ad18e2e83c8440a95d9b7c114fea568b2dfc
Gisrobin <gisrobinezeobi@gmail.com>
2025-06-05 10:51:52 -0700

    local changes before pulling

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
-          <h1>swap and sale</h1>
-      
+
+          <h1 style={{color: "black  ",textDecoration:"underline"}}>swap and sale</h1>
+
         <a
           className="App-link"
           href="https://reactjs.org"
