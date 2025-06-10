e59ccb9d8a56f6e58d59ff7603fd7f0679973bce
Gisrobin <gisrobinezeobi@gmail.com>
2025-06-10 12:38:08 -0700

    onboarding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Signin from './Components/Signin';
+import Dashboard from './context/dashboard';
+import { Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    <Signin/>
+    <>
+      <Signin/>
+      <Dashboard/>
+    </>
   );
 }
-
 export default App;
